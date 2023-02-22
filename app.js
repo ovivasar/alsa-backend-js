19377ecf3501dab8fc01c90fddff128a1ddf4bc0
ovivasar <ovivasar@gmail.com>
2023-02-22 01:08:37 -0500

    Modificacion 03 alsa backend moviendo el app.js para afuera y el package.json modificado la ruta del app.js

---
+++ app.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const res = require('express/lib/response');
+const morgan = require('morgan');
+const cors = require('cors');
+
+const ocargadetRoutes = require('./src/routes/ocargadet.routes');
+const ocargaRoutes = require('./src/routes/ocarga.routes');
+const ventadetRoutes = require('./src/routes/ventadet.routes');
+const ventaRoutes = require('./src/routes/venta.routes');
+const productoRoutes = require('./src/routes/producto.routes');
+const usuarioRoutes = require('./src/routes/usuario.routes');
+const correntistaRoutes = require('./src/routes/correntista.routes');
+const zonaRoutes = require('./src/routes/zona.routes');
+const zonadetRoutes = require('./src/routes/zonadet.routes');
+const formapagoRoutes = require('./src/routes/formapago.routes');
+
+const app = express();
+
+app.use(cors()); //comunica con otro backend
+
+app.use(morgan('dev'));
+app.use(express.json()); //para reconocer json en express, parametros POST
+
+app.use(ocargadetRoutes);
+app.use(ocargaRoutes);
+app.use(ventadetRoutes);
+app.use(ventaRoutes);
+app.use(productoRoutes);
+app.use(usuarioRoutes);
+app.use(correntistaRoutes);
+app.use(zonaRoutes);
+app.use(zonadetRoutes);
+app.use(formapagoRoutes);
+
+app.use((err, req, res, next) => {
+    return res.json({
+        message: err.message
+    })
+})
+
+app.listen(4000);
+console.log("Servidor puerto 4000");
