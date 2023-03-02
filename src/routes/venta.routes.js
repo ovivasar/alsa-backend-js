a9bbab2eebde172e693928c38990ee53d21a7ac8
ovivasar <ovivasar@gmail.com>
2023-03-01 22:17:38 -0500

    agregando parametro al routes de venta

--- src/routes/venta.routes.js
+++ src/routes/venta.routes.js
@@ -3,7 +3,7 @@ const pool = require('../db');
 const router = Router();
 const {obtenerTodasVentas,obtenerTodasVentasPlan,obtenerVenta,crearVenta,actualizarVenta,eliminarVenta} = require('../controllers/venta.controllers')
 
-router.get('/venta', obtenerTodasVentas);
+router.get('/venta/:fecha_proceso', obtenerTodasVentas);//aumentado fecha_proceso
 router.get('/ventaplan/:fecha_proceso', obtenerTodasVentasPlan); //new
 router.get('/venta/:cod/:serie/:num/:elem', obtenerVenta);
 router.post('/venta', crearVenta);
