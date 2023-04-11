bd2f038768576217bd02b9aafc78df220a50c788
ovivasar <ovivasar@gmail.com>
2023-04-11 05:29:12 -0500

    ocarga guias pendientes7

--- src/controllers/ocargadet.controllers.js
+++ src/controllers/ocargadet.controllers.js
@@ -620,9 +620,8 @@ const obtenerTodasGuiasPendientes = async (req,res,next)=> {
     strSQL = strSQL + " group by ano, numero, guia03";
 
     console.log(strSQL);
-
     try {
-        const todosReg = await pool.query(strSQL,[fecha]);
+        const todosReg = await pool.query(strSQL);
         res.json(todosReg.rows);
     }
     catch(error){
