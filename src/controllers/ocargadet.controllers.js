1d06ff385ed53a97f70f30d6ca4be12c6be57466
ovivasar <ovivasar@gmail.com>
2023-04-20 09:40:30 -0500

    ocargadet update traslados8

--- src/controllers/ocargadet.controllers.js
+++ src/controllers/ocargadet.controllers.js
@@ -617,14 +617,12 @@ const actualizarOCargaTicket = async (req,res,next)=> {
 const agregarOCargaTicketTraslado = async (req,res,next)=> {
     let strSQL;
     var sAno;
-    //const {ano,numero,item} = req.params;
-
     const {
         id_empresa,     //1
         id_punto_venta, //2
         fecha2,         //3
         //ano: calculado de la fecha
-        numerob,         //4  campo numero del body
+        numero,         //4
         //item: calculado funcion postgres
         ref_documento_id,   //5
         ref_razon_social,   //6
@@ -637,11 +635,8 @@ const agregarOCargaTicketTraslado = async (req,res,next)=> {
         id_zona_entrega,    //13 ventas referencial, no visible
         zona_entrega,       //14 ventas referencial, no visible
         registrado,         //15
-        unidad_medida,      //16
-        pedido,             //17
-        ticket_tras,         //18 new
-        peso_ticket_tras,    //19 new 
-        sacos_ticket_tras    //20 new
+        unidad_medida,         //15
+        pedido              //16
         } = req.body
 
     //cuando llega con dd/mm/yyyy o dd-mm-yyyy hay que invertir el orden, sino sale invalido
@@ -690,13 +685,11 @@ const agregarOCargaTicketTraslado = async (req,res,next)=> {
     strSQL = strSQL + " ,id_zona_entrega";      //13
     strSQL = strSQL + " ,zona_entrega";         //14
     strSQL = strSQL + " ,registrado";           //15
-    strSQL = strSQL + " ,unidad_medida";        //16
-    strSQL = strSQL + " ,ctrl_insercion";       //
-    strSQL = strSQL + " ,estado";            // 
-    strSQL = strSQL + " ,e_estibadores";     //
-    strSQL = strSQL + " ,ticket_tras";       //17 newww
-    strSQL = strSQL + " ,peso_ticket_tras";  //18 newww
-    strSQL = strSQL + " ,sacos_ticket_tras"; //19 newww
+    strSQL = strSQL + " ,unidad_medida";           //16
+
+    strSQL = strSQL + " ,ctrl_insercion";       //16
+    strSQL = strSQL + " ,estado";       //17 new
+    strSQL = strSQL + " ,e_estibadores";       //18 neww
     strSQL = strSQL + " )";
     strSQL = strSQL + " VALUES";
     strSQL = strSQL + " (";
@@ -730,14 +723,11 @@ const agregarOCargaTicketTraslado = async (req,res,next)=> {
     strSQL = strSQL + " ,$13";
     strSQL = strSQL + " ,$14";
     strSQL = strSQL + " ,$15";
-    strSQL = strSQL + " ,$16"; //unidad_medida
+    strSQL = strSQL + " ,$16"; //new unidad_medida
     strSQL = strSQL + " ,current_timestamp";
     strSQL = strSQL + " ,'PENDIENTE'";
     strSQL = strSQL + " ,'-'"; //new estibadores, para evitar el null en filtro principal
-    
-    strSQL = strSQL + " ,$17"; //new ticket_tras
-    strSQL = strSQL + " ,$18"; //new peso_ticket_tras
-    strSQL = strSQL + " ,$19"; //new sacos_ticket_tras
+
     strSQL = strSQL + " ) RETURNING *";
     try {
         console.log(strSQL);
@@ -747,7 +737,7 @@ const agregarOCargaTicketTraslado = async (req,res,next)=> {
             id_punto_venta,  //2
             fecha2,          //3
             //ano: calculado de la fecha
-            numerob,          //4
+            numero,          //4
             //item: calculado funcion postgres
             ref_documento_id,   //5
             ref_razon_social,   //6
@@ -760,14 +750,10 @@ const agregarOCargaTicketTraslado = async (req,res,next)=> {
             id_zona_entrega,    //13
             zona_entrega,       //14
             registrado,          //15
-            unidad_medida,       //16
-            ticket_tras,         //17 new
-            peso_ticket_tras,    //18 new 
-            sacos_ticket_tras    //19 new
+            unidad_medida          //16
             ]
         );
         res.json(result.rows[0]);
-        ///////////////////////////////////////////////////////////////////////////
     }catch(error){
         //res.json({error:error.message});
         next(error)
