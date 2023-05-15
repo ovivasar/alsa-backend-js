6c20098c5b474437e1268bee4eda9d8fcc1046e7
ovivasar <ovivasar@gmail.com>
2023-05-14 20:48:32 -0500

    ocargadet tipos25

--- src/controllers/ocargadet.controllers.js
+++ src/controllers/ocargadet.controllers.js
@@ -4,6 +4,7 @@ const obtenerTodasOCargasDet = async (req,res,next)=> {
     let strSQL;
     const {ano,numero,tipo} = req.params;
     
+    console.log(ano,numero,tipo);
     strSQL = "SELECT ";
     strSQL = strSQL + "  mst_ocarga_detalle.*";
     strSQL = strSQL + " ,cast(mst_ocarga_detalle.fecha as varchar)::varchar(50) as fecha2";
@@ -14,7 +15,7 @@ const obtenerTodasOCargasDet = async (req,res,next)=> {
     strSQL = strSQL + " AND numero = $2";
     strSQL = strSQL + " AND tipo = $3";
     strSQL = strSQL + " ORDER BY item";
-
+    console.log(strSQL);
     try {
         const todosReg = await pool.query(strSQL,[ano,numero,tipo]);
         res.json(todosReg.rows);
